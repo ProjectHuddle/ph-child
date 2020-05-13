46d9b0ffff10fa28acd32263f21cdafff9b1325e
Andre Gagnon <ajgagnon@uwalumni.com>
2020-05-13 10:06:57 -0500

    Increment version.

--- ph-child.php
+++ ph-child.php
@@ -5,7 +5,7 @@
  * Plugin URI: http://projecthuddle.io
  * Description: Connect a website to ProjectHuddle
  * Author: ProjectHuddle
- * Version: 1.0.25
+ * Version: 1.0.26
  *
  * Requires at least: 4.7
  * Tested up to: 5.2.2
