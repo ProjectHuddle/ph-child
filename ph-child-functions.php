77f453e7a91f21d481267cc4b122c2912086b4b3
Sushma Kure <sushmak@bsf.io>
2021-10-13 20:11:56 +0530

    PHPCS setup

--- ph-child-functions.php
+++ ph-child-functions.php
@@ -7,23 +7,22 @@
 /**
  * Is the current user allowed to comment?
  */
-function ph_child_is_current_user_allowed_to_comment()
-{
+function ph_child_is_current_user_allowed_to_comment() {
 	// if guests are allowed, yes, they are
-	if (get_option('ph_child_allow_guests', false)) {
+	if ( get_option( 'ph_child_allow_guests', false ) ) {
 		return true;
 	}
 
 	// otherwise, they must be logged in
-	if (!is_user_logged_in()) {
+	if ( ! is_user_logged_in() ) {
 		return false;
 	}
 
 	// get enabled roles
-	$enabled_roles = get_option('ph_child_enabled_comment_roles', false);
+	$enabled_roles = get_option( 'ph_child_enabled_comment_roles', false );
 
 	// enable all if it hasn't been saved yet
-	if (false === $enabled_roles && is_bool($enabled_roles)) {
+	if ( false === $enabled_roles && is_bool( $enabled_roles ) ) {
 		return true;
 	}
 
@@ -32,8 +31,8 @@ function ph_child_is_current_user_allowed_to_comment()
 	$roles = $user->roles;
 
 	// if they have one of the enabled roles, they can comment
-	foreach ($roles as $role) {
-		if (in_array($role, $enabled_roles)) {
+	foreach ( $roles as $role ) {
+		if ( in_array( $role, $enabled_roles ) ) {
 			return true;
 		}
 	}
@@ -42,8 +41,8 @@ function ph_child_is_current_user_allowed_to_comment()
 }
 
 
-function ph_child_dismiss_js()
-{ ?>
+function ph_child_dismiss_js() {
+	?>
 	<script>
 		jQuery(function($) {
 			$(document).on('click', '.ph-notice .notice-dismiss', function() {
@@ -60,58 +59,56 @@ function ph_child_dismiss_js()
 			});
 		});
 	</script>
-<?php }
+	<?php
+}
 
 /**
  * Stores notice dismissing in options table
  */
-function ph_child_ajax_notice_handler()
-{
-	$type = isset($_POST['type']) ? $_POST['type'] : false;
+function ph_child_ajax_notice_handler() {
+	$type = isset( $_POST['type'] ) ? $_POST['type'] : false;
 	// Store it in the options table
-	if ($type) {
-		update_option("dismissed-$type", true);
-		update_site_option("dismissed-$type", true);
+	if ( $type ) {
+		update_option( "dismissed-$type", true );
+		update_site_option( "dismissed-$type", true );
 	}
 }
 
-add_action('wp_ajax_ph_child_dismissed_notice_handler', 'ph_child_ajax_notice_handler');
+add_action( 'wp_ajax_ph_child_dismissed_notice_handler', 'ph_child_ajax_notice_handler' );
 
 
-function ph_child_flywheel_exclusions_notice()
-{
+function ph_child_flywheel_exclusions_notice() {
 	// on wp flywheel
-	if (!defined('FLYWHEEL_CONFIG_DIR')) {
+	if ( ! defined( 'FLYWHEEL_CONFIG_DIR' ) ) {
 		return;
 	}
 
 	// dismissed notice
-	if (get_site_option('dismissed-ph-flywheel-child', false)) {
+	if ( get_site_option( 'dismissed-ph-flywheel-child', false ) ) {
 		return;
 	}
 
 	echo '<div class="notice notice-info is-dismissible ph-notice" data-notice="ph-flywheel-child">
-			<p><strong>ProjectHuddle:</strong> ' . esc_html(sprintf(__('Flywheel hosting detected!  You\'ll need to request a cache exclusion in order for project access links to work correctly.', 'ph-child'))) . '</p>
+			<p><strong>ProjectHuddle:</strong> ' . esc_html( sprintf( __( 'Flywheel hosting detected!  You\'ll need to request a cache exclusion in order for project access links to work correctly.', 'ph-child' ) ) ) . '</p>
 			<p><a href="https://help.projecthuddle.com/article/229-flywheel-client-site-cache-exclusions" target="_blank">Learn More</a></p>
 		</div>';
 	ph_child_dismiss_js();
 }
 // add_action('admin_notices', 'ph_child_flywheel_exclusions_notice');
 
-function ph_child_wpengine_exclusions_notice()
-{
+function ph_child_wpengine_exclusions_notice() {
 	// on wp engine
-	if (!defined('WPE_APIKEY')) {
+	if ( ! defined( 'WPE_APIKEY' ) ) {
 		return;
 	}
 
 	// dismissed notice
-	if (get_site_option('dismissed-ph-wp-engine-child', false)) {
+	if ( get_site_option( 'dismissed-ph-wp-engine-child', false ) ) {
 		return;
 	}
 
 	echo '<div class="notice notice-info is-dismissible ph-notice" data-notice="ph-wp-engine-child">
-			<p><strong>ProjectHuddle:</strong> ' . esc_html(sprintf(__('WPEngine hosting detected!  You\'ll need to request a cache exclusion in order for ProjectHuddle access links to work properly.', 'ph-child'))) . '</p>
+			<p><strong>ProjectHuddle:</strong> ' . esc_html( sprintf( __( 'WPEngine hosting detected!  You\'ll need to request a cache exclusion in order for ProjectHuddle access links to work properly.', 'ph-child' ) ) ) . '</p>
 			<p><a href="https://help.projecthuddle.com/article/228-wpengine-client-site-plugin-exclusions" target="_blank">Learn More</a></p>
 		</div>';
 	ph_child_dismiss_js();
