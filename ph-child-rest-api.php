d5c015d6cb24ff64a5c3dc99b3ab700bb231eca0
anuragbsf <anurags@bsf.io>
2025-05-02 02:01:42 +0530

    Enhance REST API: Add CORS support and enable search functionality for pages

--- ph-child-rest-api.php
+++ ph-child-rest-api.php
@@ -3,7 +3,7 @@
  * REST API functionality for SureFeedback Client Site
  */
 
- if ( ! defined( 'ABSPATH' ) ) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
@@ -13,6 +13,7 @@ class PH_Child_REST_API {
      */
     public function __construct() {
         add_action( 'rest_api_init', array( $this, 'register_routes' ) );
+        add_action( 'rest_api_init', array( $this, 'allow_cors' ) );
     }
 
     /**
@@ -22,7 +23,7 @@ class PH_Child_REST_API {
         register_rest_route( 'surefeedback/v1', '/pages', array(
             'methods'  => 'GET',
             'callback' => array( $this, 'get_pages' ),
-            'permission_callback' => array( $this, 'verify_access' )
+            'permission_callback' => array( $this, 'verify_access' ),
         ) );
     }
 
@@ -46,30 +47,55 @@ class PH_Child_REST_API {
     }
 
     /**
-     * Get all published pages
+     * Get all published pages with optional search
      */
     public function get_pages( WP_REST_Request $request ) {
+        $search_query = $request->get_param( 'search' );
+
         $args = array(
             'post_type'      => 'page',
             'post_status'    => 'publish',
             'posts_per_page' => -1,
             'orderby'        => 'title',
-            'order'          => 'ASC'
+            'order'          => 'ASC',
         );
+
+        if ( ! empty( $search_query ) ) {
+            $args['s'] = $search_query; // Add search parameter to query
+        }
 
         $pages = get_posts( $args );
         
         $response = array();
         foreach ( $pages as $page ) {
             $response[] = array(
-                'id'        => $page->ID,
-                'title'     => $page->post_title,
-                'url'       => get_permalink( $page->ID ),
+                'id'    => $page->ID,
+                'title' => $page->post_title,
+                'url'   => get_permalink( $page->ID ),
             );
         }
 
         return rest_ensure_response( $response );
     }
+
+    /**
+     * Allow CORS for the specified origin
+     */
+    public function allow_cors() {
+        add_filter( 'rest_pre_serve_request', array( self::class, 'cors_headers' ) );
+    }
+    
+    /**
+     * Add CORS headers
+     */
+    public static function cors_headers( $value ) {
+        header( 'Access-Control-Allow-Origin: *' );
+        header( 'Access-Control-Allow-Credentials: true' );
+        header( 'Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE' );
+        header( 'Access-Control-Allow-Headers: Content-Type, X-SureFeedback-Token, Authorization' );
+        return $value;
+    }
+    
 }
 
 // Initialize the REST API
